b2e4ef9829049d58979921d0b640afca7c435d4e
Kunxi Griswold <kgriswo1@binghamton.edu>
2019-07-22 06:34:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,9 +11,37 @@ def using_pop(array)
 end
 
 def pop_with_args(array)
-    array.pop
+    array.pop(2)
 end
 
-def pop_with_args(array)
-    array.pop
+def using_shift(array)
+    array.shift
+end
+
+def shift_with_args(array)
+    array.shift(2)
+end
+
+def using_concat(array1, array2)
+    array1.concat(array2)
+end
+
+def using_insert(array, element)
+    array.insert(4, element)
+end
+
+def using_uniq(array)
+    array.uniq
+end
+
+def using_flatten(array)
+    array.flatten
+end
+
+def using_delete(array, string)
+    array.delete(string)
+end
+
+def using_delete_at(array, num)
+    array.delete_at(num)
 end
